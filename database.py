b6bfa66a75d4ebddc96cd8f01f522087b8aa9dd9
Jurgen <jurgen.hartjes@student.hu.nl>
2023-11-07 09:34:25 +0100

    database

---
+++ database.py
@@ -0,0 +1,15 @@
+import psycopg2.extras
+def inserter(datum,bericht,station,naam, keuring, naammod, mailadres, datum_mod): #wil alle benodigde info hebben om dit in de database te schijven
+
+
+    connection_string = "host = '20.254.113.210' dbname = 'stationszuil' user = 'postgres' password = 'fj6YDB5D'"
+
+    conn = psycopg2.connect(connection_string)
+    cursor = conn.cursor()
+
+    query = f"""INSERT INTO public.berichten(bericht, naam, datums, station, keuring, naam_mod, mailadres, datum_mod)
+        VALUES ('{bericht}', '{naam}', '{datum}','{station}', '{keuring}', '{naammod}', '{mailadres}', '{datum_mod}');""" #voegt de benodigde variabele van het moderatie bestand toe
+
+    cursor.execute(query)
+    conn.commit() #verstuurt de informatie naar de database
+    conn.close()
